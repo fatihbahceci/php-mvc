29b7ce2c2dde16a480e0eb1f4bbfbeff7b16d2b6
fatih <fatih@underground>
2019-03-06 01:42:38 +0300

    sesion and h tag

--- core/lib.php
+++ core/lib.php
@@ -10,7 +10,6 @@ class str
     {
         return mb_strtoupper($str, "UTF-8");
     }
-
 }
 
 class srv
@@ -36,7 +35,7 @@ class arrays
         return array_key_exists($key, $array);
     }
 
-    public static function isNullOrEmpty(array $a)
+    public static function isNullOrEmpty(array $a = null)
     {
         return $a == null || count($a) <= 0;
     }
@@ -49,7 +48,16 @@ class tags
         if ($return) {
             return "<pre>" . print_r($s, true) . "</pre>";
         } else {
-            echo ("<pre>" . print_r($s,true) . "</pre>");
+            echo ("<pre>" . print_r($s, true) . "</pre>");
+        }
+    }
+
+    public static function h($s, $level = 1, $return = false)
+    {
+        if ($return) {
+            return "<h$level>" . print_r($s, true) . "</h$level>";
+        } else {
+            echo ("<h$level>" . print_r($s, true) . "</h$level>");
         }
     }
 }
@@ -66,6 +74,54 @@ class sys
         $t = explode(" ", microtime(false));
         //0.21385600 1546067709
         return $t[1] . substr($t[0], 2);
-
     }
 }
+
+
+class session
+{
+    //10 DK
+    //public static $TIMEOUT = 10 * 60;
+    private static function checkTimeout()
+    {
+        if (defined('_session_timeout')) {
+            if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > _session_timeout)) {
+                // last request was more than [_session_timeout] seconds ago
+                session_unset(); // unset $_SESSION variable for the run-time
+                session_destroy(); // destroy session data in storage
+            }
+            $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+        }
+
+        //You can also use an additional time stamp to regenerate the session ID periodically to avoid attacks on sessions like session fixation:
+        /*
+    if (!isset($_SESSION['CREATED'])) {
+    $_SESSION['CREATED'] = time();
+    } else if (time() - $_SESSION['CREATED'] > 1800) {
+    // session started more than 30 minutes ago
+    session_regenerate_id(true); // change session ID for the current session and invalidate old session ID
+    $_SESSION['CREATED'] = time(); // update creation time
+    }
+     */
+    }
+    public static function get(string $key)
+    {
+        static::checkTimeout();
+        session_start();
+        return $_SESSION[$key];
+    }
+
+    public static function getAndDelete(String $key) {
+        $r = static::get($key);
+        static::set($key,null);
+        return $r;
+    }
+
+    public static function set(string $key, $value)
+    {
+        static::checkTimeout();
+        session_start();
+        $_SESSION[$key] = $value;
+    }
+}
+ 
